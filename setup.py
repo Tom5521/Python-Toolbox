ada990aaf2827a4f41499348f27fdfbaa2d73801
Tom5521 <88908582+Tom5521@users.noreply.github.com>
2023-04-14 04:03:13 -0400

    Update

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
-    name="Tom Toolbox",
-    version="0.0.1",
+    name="Tom-Toolbox",
+    version="0.0.1-1",
     author="Tom5521",
     description="A collection of utilities for python 3.10 and above",
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Tom5521/Toolbox",
     packages=setuptools.find_packages(exclude=["test.py", ".gitignore"]),
