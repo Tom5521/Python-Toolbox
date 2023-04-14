5439b09d395d6f8f98477f3af00f40c00dd947e5
Tom5521 <88908582+Tom5521@users.noreply.github.com>
2023-04-14 04:51:45 -0400

    Update

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import setuptools
 
 setuptools.setup(
     name="Tom-Toolbox",
-    version="0.0.1-1",
+    version="0.0.1-2",
     author="Tom5521",
     description="A collection of utilities for python 3.10 and above",
-    long_description=open('README.md').read(),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Tom5521/Toolbox",
     packages=setuptools.find_packages(exclude=["test.py", ".gitignore"]),
